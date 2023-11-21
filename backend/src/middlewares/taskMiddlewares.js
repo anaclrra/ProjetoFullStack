5df5826293d47c4bb0de9ed03254161b73713a58
anaclrra <anaclarab826@gmail.com>
2023-11-21 12:58:45 -0300

    add middleware user

--- backend/src/middlewares/taskMiddlewares.js
+++ backend/src/middlewares/taskMiddlewares.js
@@ -1,6 +1,6 @@
 const validateBody = (req, res, next) => {
     const { body } = req;
-   
+
     if (body.title === undefined || body.descricao === undefined || body.date === undefined) {
         return res.status(400).json({ message: 'The fields is required' });
     }
@@ -10,6 +10,19 @@ const validateBody = (req, res, next) => {
     next();
 };
 
+const validateBodyUser = (req, res, next) => {
+    const { body } = req;
+
+    if (body.name === undefined || body.email === undefined || body.password === undefined) {
+        return res.status(400).json({ message: 'The fields is required' });
+    }
+    if (body.name === '' || body.email === '' || body.password === '') {
+        return res.status(400).json({ message: 'The fields cannot be empty' });
+    }
+    next();
+};
+
 module.exports = {
-    validateBody
+    validateBody,
+    validateBodyUser
 }
