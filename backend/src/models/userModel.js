b575af42c58647801a1d158b7a160c0ccc186e0e
anaclrra <anaclarab826@gmail.com>
2023-11-21 12:48:15 -0300

    add model user

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,82 @@
+const connection = require('./connection')
+
+const getAll = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM users', (error, results) => {
+            if (error) {
+                console.error('Erro na consulta SQL:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const getId = (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM users WHERE id = ?', [id], (error, results) => {
+            if (error) {
+                console.error('Erro na consulta SQL:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const creatUser = (user) => {
+    const { name, email, password } = user;
+
+    return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO users (username, email, passwrd) VALUES(?,?,?)',
+            [name, email, password], (error, results) => {
+                if (error) {
+                    console.error('Erro na consulta SQL:', error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+    });
+
+};
+
+const deleteUser = async (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query('DELETE FROM users WHERE id = ?',
+            [id], (error, results) => {
+                if (error) {
+                    console.error('Erro na consulta SQL:', error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+    });
+};
+
+const updateUser = async (id, user) => {
+    const { email, password } = user;
+
+    return new Promise((resolve, reject) => {
+        connection.query('UPDATE users SET email = ?, passwrd = ? WHERE id = ?',
+            [email, password, id], (error, results) => {
+                if (error) {
+                    console.error('Erro na consulta SQL:', error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+    });
+};
+
+module.exports = {
+    getAll,
+    getId,
+    creatUser,
+    deleteUser,
+    updateUser
+}
