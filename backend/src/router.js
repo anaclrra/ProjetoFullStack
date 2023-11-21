b67f5ef520c61b376c2257e4c0944bca8766e074
anaclrra <anaclarab826@gmail.com>
2023-11-21 13:02:02 -0300

    add routers user

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const taskController = require('./controllers/taskControllers')
-const taskMiddleware = require('./middlewares/taskMiddlewares')
+const userController = require('./controllers/userController')
+const middleware = require('./middlewares/taskMiddlewares')
 
 const router = express.Router();
 
 router.get('/tasks', taskController.getAll);
+router.get('/users', userController.getAll);
+
 router.get('/tasks/:id', taskController.getId);
-router.post('/tasks', taskMiddleware.validateBody, taskController.creatTask);
+router.get('/users/:id', userController.getId);
+
+router.post('/tasks', middleware.validateBody, taskController.creatTask);
+router.post('/users', middleware.validateBodyUser, userController.creatUser);
+
 router.delete('/tasks/:id', taskController.deleteTask);
-router.put('/tasks/:id', taskMiddleware.validateBody, taskController.updateTask);
+router.delete('/users/:id', userController.deleteUser);
+
+router.put('/tasks/:id', middleware.validateBody, taskController.updateTask);
+router.put('/users/:id', middleware.validateBodyUser, userController.updateUser);
 
 
 module.exports = router;
