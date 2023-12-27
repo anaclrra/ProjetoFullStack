59139b0e2cf2d7e95346c0eec39708b4720d5100
Ana Clara <anaclarab826@gmail.com>
2023-12-26 22:01:40 -0300

    resolvendo bug de connection

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
-require('dotenv').config();
+
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'dbtask2'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 });
 
 module.exports = connection;
